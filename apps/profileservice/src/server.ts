498b9cd757499b7f9d55441adb0834ef29fb1ce9
Dobri <dobri@d-centralize.nl>
2023-03-08 23:55:42 +0100

    feat: profile service

---
+++ apps/profileservice/src/server.ts
@@ -0,0 +1,19 @@
+import { json, urlencoded } from "body-parser";
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+export const createServer = () => {
+  const app = express();
+  app
+    .disable("x-powered-by")
+    .use(morgan("dev"))
+    .use(urlencoded({ extended: true }))
+    .use(json())
+    .use(cors())
+    .get("/healthz", (req, res) => {
+      return res.json({ ok: true });
+    });
+
+  return app;
+};
