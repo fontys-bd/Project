e367b301201a8416a6f0d94d5647467aa87ee46d
Dobri <dobri@d-centralize.nl>
2023-03-09 03:32:08 +0100

    feat: Profile static/dynamic routes

--- apps/profileservice/src/index.ts
+++ apps/profileservice/src/index.ts
@@ -1,10 +1,12 @@
+import { userRouter } from "./routes/users";
 import { createServer } from "./server";
 import { log } from "logger";
 
-// eslint-disable-next-line turbo/no-undeclared-env-vars
 const port = process.env.PORT || 3002;
 const server = createServer();
 
 server.listen(port, () => {
   log(`api running on ${port}`);
 });
+
+server.use("/user", userRouter());
