cae222db133d28029b167d47ab26d13d9857602b
Dobri <79987000+NehuaV@users.noreply.github.com>
2023-03-27 21:40:01 +0200

    fix(userservice): formatting

--- apps/user-service/src/service/index.ts
+++ apps/user-service/src/service/index.ts
@@ -1,16 +1,16 @@
 import * as prisma from "../integration/index";
 
 export async function GetUsers() {
-  const users =  await prisma.GetUsers();
+  const users = await prisma.GetUsers();
 
   return users;
 }
 
 export async function GetUsersById(id: string) {
   const user = await prisma.GetUserByID(id);
-  console.log("user service :",user);
+  console.log("user service :", user);
 
- return user;
+  return user;
 }
 
 export async function CreateUser(data: any) {
