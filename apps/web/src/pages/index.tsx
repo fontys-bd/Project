a750ed4c4e22d601da751a938f1aa3eca0a5c17f
KrisKolev <k.kolev@student.fontys.nl>
2023-03-30 00:24:47 +0200

    Added: basic login form

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,11 +1,68 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import "firebase/auth";
+import "firebase/storage";
+import "firebase/firestore";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC1u9YMqY_fG-C4xCq62hAPiRQMFK7Nt18",
+  authDomain: "blinddate-381513.firebaseapp.com",
+  projectId: "blinddate-381513",
+  storageBucket: "blinddate-381513.appspot.com",
+  messagingSenderId: "802932977941",
+  appId: "1:802932977941:web:710b86663c664be865791c",
+};
 
 export default function Main() {
+  const router = useRouter();
+  const app = initializeApp(firebaseConfig);
+
+  function handleClick() {
+    router.push("/home");
+  }
+
   return (
     <main className="grid h-screen place-content-center">
-      <Link href={"/home"} className="text-4xl">
+      {/* <Link href={"/home"} className="text-4xl">
         Login HERE!
-      </Link>
+      </Link> */}
+
+      <style jsx global>{`
+        body {
+          background-image: url("/background.png");
+          background-size: cover;
+        }
+      `}</style>
+
+      <img
+        src="/blind_date_logo(1).png"
+        alt="Logo"
+        className="h-250 mb-4 w-80 rounded-lg"
+      />
+
+      <form className="mb-4">
+        <input
+          type="text"
+          className="w-full rounded-md border border-gray-500 px-4 py-2 text-center"
+          placeholder="Enter your email"
+        />
+      </form>
+
+      <form className="mb-4">
+        <input
+          type="text"
+          className="w-full rounded-md border border-gray-500 px-4 py-2 text-center"
+          placeholder="Enter your password"
+        />
+      </form>
+
+      <button
+        onClick={handleClick}
+        className="rounded bg-pink-400 py-2 px-4 font-bold text-white hover:bg-pink-600"
+      >
+        Login
+      </button>
     </main>
   );
 }
