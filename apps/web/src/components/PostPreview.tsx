65b786d92b8b74896277b8c3304fcea2f84f9ca8
Kristiyan Balev <kristiyan.balev@gmail.com>
2023-04-19 18:07:01 +0200

    home post order fixed, date calculation fixed and small css update

--- apps/web/src/components/PostPreview.tsx
+++ apps/web/src/components/PostPreview.tsx
@@ -2,8 +2,6 @@ import Link from "next/link";
 import { AiOutlineLike, AiOutlineDislike } from "react-icons/ai";
 import { IoBookmarkOutline } from "react-icons/io5";
 import { BiUserCircle } from "react-icons/bi";
-import formatDistanceToNow from "date-fns/formatDistanceToNow";
-import { enUS } from "date-fns/locale";
 
 interface PostProps {
   id: string;
@@ -15,15 +13,41 @@ interface PostProps {
 }
 
 export default function PostPreview(props: PostProps) {
-  const originalDate = new Date(props.created_at);
-  const elapsed = Date.now() - originalDate.getTime();
-  const elapsedStr = formatDistanceToNow(elapsed, {
-    addSuffix: true,
-    locale: enUS,
-  });
+  const postDate = new Date(props.created_at);
+  let displayDate;
+  const currentDate = new Date();
+
+  const timeDiff = currentDate.getTime() - postDate.getTime();
+
+  //calculate number of minutes that have passed
+  const minutesDiff = Math.floor(timeDiff / (1000 * 60));
+
+  // calculate number of hours that have passed
+  const hoursDiff = Math.floor(timeDiff / (1000 * 60 * 60));
+
+  // calculate number of days that have passed
+  const daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+
+  // calculate number of months that have passed
+  const monthsDiff = (currentDate.getFullYear() - postDate.getFullYear()) * 12 + (currentDate.getMonth() - postDate.getMonth());
+
+  // calculate number of years that have passed
+  const yearsDiff = currentDate.getFullYear() - postDate.getFullYear();
+
+  if (minutesDiff < 60) {
+    displayDate = minutesDiff + " minutes ago";
+  } else if (hoursDiff < 24) {
+    displayDate = hoursDiff + " hours ago";
+  } else if (daysDiff < 30) {
+    displayDate = daysDiff + " days ago";
+  } else if (monthsDiff < 12) {
+    displayDate = monthsDiff + " months ago";
+  } else {
+    displayDate = yearsDiff + " years ago";
+  }
 
   return (
-    <Link href={`/home/${props.id}`}>
+    <Link href={`/home/ ${props.id}`}>
       <article className="mx-3 mb-3 rounded-xl border border-solid border-gray-200 bg-white px-5 transition ease-in-out hover:border-gray-100 hover:shadow-lg">
         <h1 className="mt-2 flex items-center text-xs">
           Posted by:
@@ -34,28 +58,30 @@ export default function PostPreview(props: PostProps) {
             }}
           />
           <p className="ml-1 mr-3 text-sm font-bold">{props.username}</p>
-          <span className="text-gray-500">{elapsedStr}</span>
-          <p
-            className={`ml-auto text-lg ${
-              props.status === "ACTIVE"
+          <span className="text-gray-500">{displayDate}</span>
+          <span className="ml-auto text-lg flex flex-row">
+            <p className="mt-1 flex items-center text-xs">Status:</p>
+            <p
+              className={`px-2 ${props.status === "ACTIVE"
                 ? "text-green-600"
                 : props.status === "URGENT"
-                ? "text-yellow-500"
-                : "text-red-600"
-            }`}
-          >
-            {props.status}
-          </p>
+                  ? "text-yellow-500"
+                  : "text-red-600"
+                } `}
+            >
+              {props.status}
+            </p>
+          </span>
         </h1>
         <div className="my-3 text-base">{props.title}</div>
-        <section className="mb-3 flex w-full flex-row">
+        <section className="mb-3 flex w-full flex-row font-semibold">
           <button className="flex items-center">
             <AiOutlineLike
               style={{
                 fontSize: "1.5rem",
               }}
             />
-            <p className="mx-2">Like</p>
+            <p className="mx-1">Like</p>
           </button>
           <button className="flex items-center">
             <AiOutlineDislike
@@ -64,7 +90,7 @@ export default function PostPreview(props: PostProps) {
                 marginLeft: "0.5rem",
               }}
             />
-            <p className="mx-2">Dislike</p>
+            <p className="mx-1">Dislike</p>
           </button>
           <button className="ml-auto flex items-center">
             <IoBookmarkOutline
@@ -72,7 +98,7 @@ export default function PostPreview(props: PostProps) {
                 fontSize: "1.5rem",
               }}
             />
-            <p className="mx-2">Save</p>
+            <p className="mx-1">Save</p>
           </button>
         </section>
       </article>
