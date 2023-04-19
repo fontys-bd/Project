5363bece17a063e558bcac23c6e44a87b17a42c3
Kristiyan Balev <kristiyan.balev@gmail.com>
2023-04-19 19:11:21 +0200

    fixing web app communication with services in docker

--- apps/web/src/api/posts.ts
+++ apps/web/src/api/posts.ts
@@ -1,7 +1,10 @@
 import { Post } from "../types/post";
 
 export const fetchPosts = async (): Promise<Post[]> => {
-  const response = await fetch("http://localhost:3003/post");
+  const response = await fetch(
+    process.env.NEXT_PUBLIC_POST_SERVICE_URL + "/post" ||
+      "http://localhost:3003/post"
+  );
   const posts = await response.json();
   return posts;
 };
