d925026233f1e55fc2a30aa704e19b76249c40ba
Dobri <79987000+NehuaV@users.noreply.github.com>
2023-05-09 23:05:42 +0200

    fix .env bs

--- apps/user-service/src/index.ts
+++ apps/user-service/src/index.ts
@@ -2,12 +2,11 @@ import { userRouter } from "./routes";
 import { createServer } from "./server";
 import { log } from "logger";
 
-const port = process.env.NEXT_PUBLIC_USER_SERVICE_URL || 3002;
-const endPoint = process.env.NODE_ENV === "production" ? "/" : "/user";
+const port = process.env.NEXT_PUBLIC_USER_SERVICE_URL?.split(":")[2] || 3002;
 const server = createServer();
 
 server.listen(port, () => {
   log(`api running on ${port}`);
 });
 
-server.use(endPoint, userRouter());
+server.use("", userRouter());
