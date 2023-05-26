37297fb4e7f842c309e5a8b9a44624af66339741
stassig <stassig164@gmail.com>
2023-05-27 01:07:19 +0200

    cors fix

--- apps/web/src/utils/fetcher.ts
+++ apps/web/src/utils/fetcher.ts
@@ -2,9 +2,11 @@ export const fetcher = async (url: string): Promise<any> => {
   const response = await fetch(url, {
     headers: {
       "Content-Type": "application/json",
+      "ngrok-skip-browser-warning": "69420",
       Authorization:
         "Bearer " + (await fetch("/api/auth/token").then((res) => res.json())),
     },
+    credentials: "include",
   });
   return await response.json();
 };
