cdabeacfe5fab6c7fa04310ad66aed3440d6a5fe
Dobri <Dobri.trifonov1@gmail.com>
2023-06-11 00:12:36 +0200

    fix test

--- apps/post-service/src/server.ts
+++ apps/post-service/src/server.ts
@@ -11,6 +11,9 @@ export const createServer = () => {
     .use(urlencoded({ extended: true }))
     .use(json())
     .use(cors())
+    .get("/message/:name", (req, res) => {
+      return res.json({ message: `hello ${req.params.name}` });
+    })
     .get("/healthz", (req, res) => {
       res.set("Cache-Control", "s-maxage=600");
       return res.json({ ok: true });
